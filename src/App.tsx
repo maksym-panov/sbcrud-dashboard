d93d62602538577791738cf14d453a4d60e24e99
Maksym Panov <maksym.panov@hneu.net>
2024-05-14 15:13:14 +0300

    Init project structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <header>
+      </header>
+      <main>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
